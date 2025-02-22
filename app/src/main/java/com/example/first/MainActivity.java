b60a8e8b8f066f399e49bd107f1c983600f915f6
Pauline <some@mail>
2025-02-22 09:47:58 +0300

    created color changer

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.first;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private Integer counter = 0;
+    private Integer colorCount = 0;
 
     public void onClickBoxes(View view){
         counter++;
         TextView textView = findViewById(R.id.some);
         textView.setText(counter.toString());
+    }
+    public void onClickColorChange(View view){
+        ConstraintLayout backg = findViewById(R.id.main);
+        if(colorCount==3){
+            colorCount=1;
+        }else{
+            colorCount++;
+        }
+        switch (colorCount){
+            case 1:
+                backg.setBackgroundColor(Color.parseColor("#ff0000"));
+                break;
+            case 2:
+                backg.setBackgroundColor(Color.parseColor("#ffff00"));
+                break;
+            case 3:
+                backg.setBackgroundColor(Color.parseColor("#00ff00"));
+                break;
+        }
+
     }
 
     @Override
@@ -63,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outstate){
         super.onSaveInstanceState(outstate);
         outstate.putInt("what", counter);
+        outstate.putInt("what2", colorCount);
         Log.d("Instance", "Saved");
     }
     @Override
@@ -70,6 +95,10 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstance);
         if(savedInstance!=null && savedInstance.containsKey("what")){
             counter=savedInstance.getInt("what");
+            Log.d("Instance", "Get");
+        }
+        if(savedInstance!=null && savedInstance.containsKey("what2")){
+            colorCount=savedInstance.getInt("what2");
             Log.d("Instance", "Get");
         }
     }
